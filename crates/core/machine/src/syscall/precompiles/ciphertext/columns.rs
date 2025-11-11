9af0aa223263aaaaa7cfe21e9106f4ee21efdbc2
vanhger <vietanhpg2003@gmail.com>
2025-11-11 08:48:43 +0700

    fix: air columns

--- crates/core/machine/src/syscall/precompiles/ciphertext/columns.rs
+++ crates/core/machine/src/syscall/precompiles/ciphertext/columns.rs
@@ -1,6 +1,6 @@
 use crate::memory::{MemoryReadCols, MemoryWriteCols};
 use zkm_derive::AlignedBorrow;
-use crate::operations::XorOperation;
+use crate::operations::{IsEqualWordOperation, XorOperation};
 
 /// CiphertextCheckCols is the column layout for the Ciphertext check.
 /// The number of rows equal to the number of gates
@@ -15,14 +15,15 @@ pub struct CiphertextCheckCols<T> {
     pub receive_syscall: T,
     pub is_first_gate: T,
     pub is_last_gate: T,
+    pub not_last_gate: T,
     pub gates_id: T,
     pub gate_input_mem: [MemoryReadCols<T>; 16],
     pub num_gate_mem: MemoryReadCols<T>,
     pub result_mem: MemoryWriteCols<T>,
     pub inter1: [XorOperation<T>; 4], // h1 ^ h0
     pub inter2: [XorOperation<T>; 4], // h1 ^ h0 ^ label_b
-    pub check: T,
-
+    pub is_equal_words: [IsEqualWordOperation<T>; 4], // computed ciphertext == expected_ciphertext
+    pub checks: [T; 4], // check result for each pair of is_equal_words
 }
 
 pub const NUM_CIPHERTEXT_CHECK_COLS: usize = size_of::<CiphertextCheckCols<u8>>();
