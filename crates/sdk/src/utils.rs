a0d175451f7ba048b41f83aa34beacdfeed87070
Snezhkko <snezhkodaria38@gmail.com>
2025-12-29 14:02:09 +0200

    fix(sdk): remove unnecessary clone in zkm_dump (#393)

--- crates/sdk/src/utils.rs
+++ crates/sdk/src/utils.rs
@@ -15,7 +15,7 @@ pub(crate) fn zkm_dump(elf: &[u8], stdin: &ZKMStdin) {
     if std::env::var("ZKM_DUMP").map(|v| v == "1" || v.to_lowercase() == "true").unwrap_or(false) {
         std::fs::write("program.bin", elf).unwrap();
         let stdin = bincode::serialize(&stdin).unwrap();
-        std::fs::write("stdin.bin", stdin.clone()).unwrap();
+        std::fs::write("stdin.bin", &stdin).unwrap();
     }
 }
 
